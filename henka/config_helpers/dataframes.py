c78311ed1ca3a1e51d66ed7655ac109ed3ad6755
pedro <pgarcia14180@gmail.com>
2019-10-16 11:41:34 -0300

    Allow serie and df objects

--- henka/config_helpers/dataframes.py
+++ henka/config_helpers/dataframes.py
@@ -1,6 +1,7 @@
-from .source import get_dataframe_from_source, get_dataframe_from_dataframe_source, JoinSource, AggSource, AggConcatenatedSource
+from .source import get_dataframe_from_source, get_dataframe_from_dataframe_source, JoinSource, AggSource, AggConcatenatedSource, SerieSource
 from .process import DataframeProcessConfig
 from .save import DataframeSave
+from henka.utils.dictionary import DictToClass
 
 class DataframeConfig:
     def __init__(self, **kwargs):
@@ -13,11 +14,15 @@ class DataframeConfig:
             self.source = AggSource(**source_dict)
         elif self.source_name == 'agg_concatenated':
             self.source = AggConcatenatedSource(**source_dict)
+        elif self.source_name == 'serie':
+            self.source = SerieSource(**source_dict)
+        elif self.source_name == 'df':
+            self.source = DictToClass(**source_dict)
         else:
             self.dataframe = get_dataframe_from_source(**kwargs['source'])
         self.process = DataframeProcessConfig(**kwargs['process'])
         if 'save' in kwargs:
-            self.save = DataframeSave(**kwargs['save'])
+            self.save = DataframeSave(_dataframe_name = self.name, **kwargs['save'])
 
     def set_df_from_dataframe(self, dataframes):
         self.dataframe = get_dataframe_from_dataframe_source(dataframes, self.source)
