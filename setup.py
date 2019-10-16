336db96ca1bc390d648af88ce639e48eb8bb9e71
pedro <pgarcia14180@gmail.com>
2019-10-16 11:58:12 -0300

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="henka",
-    version="0.0.1",
+    version="1.0.0",
     author="Pedro Garcia",
     author_email="pgarcia14180@gmail.com",
     description="Henka Lib",
