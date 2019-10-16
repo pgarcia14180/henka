53abdd9d1336d23d6e73d0343638c5bdb5a997c1
pedro <pgarcia14180@gmail.com>
2019-10-16 16:25:48 -0300

    Upgrade henka version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="henka",
-    version="1.0.0",
+    version="1.0.1",
     author="Pedro Garcia",
     author_email="pgarcia14180@gmail.com",
     description="Henka Lib",
