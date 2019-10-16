2caf4bf55c0cf59f468fe5d759aa89f2a4512f03
pedro <pgarcia14180@gmail.com>
2019-10-16 11:41:04 -0300

    Separate henka functions from the main function

--- henka/processes/henka.py
+++ henka/processes/henka.py
@@ -8,37 +8,36 @@ def remove_duplicates(df, config):
     return df.drop_duplicates(config.remove_duplicates, keep='first')
 
 def duplicate_columns(df, config):
-    for k, v in config.duplicate_columns.items():
+    for k, vs in config.duplicate_columns.items():
+        for v in vs:
             df[v] = df[k]
+    return df
+
+def fill_empty(df, config):
+    """
+    Parameter must be either a value or a dictionary.
+    The format of the dictionary is key being the column name, and the value the value to fill the nan.
+    When only a value is sent this will fill the whole dataframe if an empty value is found
+    """
+    if isinstance(config.fill_empty, dict):
+        for k, v in config.fill_empty.items():
+            df[k] = df[k].fillna(v) 
+    else:
+        df = df.fillna(config.fill)
     return df
 
 def mock(df, config):
     for column, value in config.mock.items():
-        serie =  pd.Series([value for _ in range(0, df.shape[0])])
+        df[column] = value
+        """ serie =  pd.Series([value for _ in range(0, df.shape[0])])
         serie_df = pd.DataFrame(serie)
         serie_df = serie_df.rename(columns={0: column})
-        df = df.join(serie_df)
+        df = df.join(serie_df) """ 
     return df
 
 def columns(df, config):
     if bool(config.columns):
         df = df[config.columns]
-    return df
-
-def remove_duplicates(df, config):
-    return df.drop_duplicates(config.remove_duplicates, keep='first')
-
-def duplicate_columns(df, config):
-    for k, v in config.duplicate_columns.items():
-        df[v] = df[k]
-    return df
-
-def mock(df, config):
-    for column, value in config.mock.items():
-        serie =  pd.Series([value for _ in range(0, df.shape[0])])
-        serie_df = pd.DataFrame(serie)
-        serie_df = serie_df.rename(columns={0: column})
-        df = df.join(serie_df)
     return df
 
 def replace_content(df, config):
@@ -62,13 +61,13 @@ def remove_row(df, config):
     return df
 
 def process_content(df, config):
-    for function_name, columns in config.process_content.items():
-        for column in columns:
-            df[column]= df[column].map(config.function_config[function_name])
+    for function_dict in config.process_content:
+        for column in function_dict['columns']:
+            df[column]= df[column].map(function_dict['function'])
     return df
 
-def process_content_custom(df, config):
-    for function_dict in config.process_content_custom:
+def formulate(df, config):
+    for function_dict in config.formulate:
         function = set_columns_from_arguments(df, function_dict['function']) 
         df[function_dict['result']] = np.vectorize(function)()
     return df
@@ -96,6 +95,12 @@ def delete_columns(df, config):
         df = df.drop(columns=[to_delete_column])
     return df
 
+def keep_columns(df, config):
+    to_delete_columns = set(df.columns.values).difference(config.keep_columns)
+    for to_delete_column in to_delete_columns:
+        df = df.drop(columns=[to_delete_column])
+    return df
+
 def rename_columns(df, config):
     if bool(config.rename_columns):
         df = df.rename(columns=config.rename_columns)
@@ -104,19 +109,22 @@ def rename_columns(df, config):
 henka_functions = {
     'remove_duplicates':remove_duplicates,
     'duplicate_columns':duplicate_columns,
-    'columns':columns,
+    'keep_columns':keep_columns,
+    'remove_row': remove_row,
     'mock':mock,
     'replace_content':replace_content,
     'keep_row':keep_row,
     'process_content':process_content,
-    'process_content_custom':process_content_custom,
+    'formulate':formulate,
     'concatenate_columns':concatenate_columns,
     'delete_columns':delete_columns,
     'rename_columns':rename_columns,
+    'fill_empty': fill_empty,
 }
 
 def process_dataframe(dataframe_config, dataframes):
-    if dataframe_config.source_name in ['agg', 'agg_concatenated', 'df', 'join']:
+    print(dataframe_config.name)
+    if dataframe_config.source_name in ['agg', 'agg_concatenated', 'df', 'join', 'serie']:
         dataframe_config.set_df_from_dataframe(dataframes)
     df = dataframe_config.dataframe
     config = dataframe_config.process
@@ -124,9 +132,6 @@ def process_dataframe(dataframe_config, dataframes):
     drop = dataframe_config.drop if hasattr(dataframe_config, 'drop') else None
 
     for function_name in config._source.keys():
-        if function_name == 'function_config':
-            continue
-        print(function_name)
         df = henka_functions[function_name](df, config)
 
     if save:
@@ -137,8 +142,10 @@ def process_dataframe(dataframe_config, dataframes):
 
 
 
-def henka(config):
+def henka(config, **kwargs):
     dataframes = {}
+    if kwargs:
+        dataframes.update(**kwargs)
     for dataframe_config in config.dataframes:
         df = process_dataframe(dataframe_config, dataframes)
         if df:
