850a01a272deaf414e70a818012f8b239018f460
Pedro García <pgarcia14180@gmail.com>
2020-06-12 14:31:28 -0400

    update ci flow

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+import os
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+version = os.environ.get('PYPI_VERSION') or 'latest'
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = lib_folder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setuptools.setup(
     name="henka",
-    version="1.0.2",
+    version=version,
     author="Pedro Garcia",
     author_email="pgarcia14180@gmail.com",
     description="Henka Lib",
@@ -13,21 +22,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://cencoreg.cencosud.corp/repository/pyprod/",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'elasticsearch==6.3.1',
-        'numpy==1.16.3',
-        'pandas==0.24.2',
-        'protobuf==3.8.0',
-        'python-dateutil==2.8.0',
-        'pytz==2019.1',
-        'six==1.12.0',
-        'urllib3==1.25.2',
-        'Unidecode==1.0.23',
-        'xlrd==1.2.0',
-        'et-xmlfile==1.0.1',
-        'jdcal==1.4.1',
-        'openpyxl==2.6.3',
-    ],
+    install_requires= install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
